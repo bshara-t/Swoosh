56f88ef954cad2743c80cce0d6242c5799af8815
bshara <bshara14@hotmail.co.il>
2019-02-16 14:51:53 +0200

    lesson 4

--- app/src/main/java/com/repoai/bshara/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/repoai/bshara/swoosh/LeagueActivity.kt
@@ -1,12 +1,23 @@
 package com.repoai.bshara.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_league.*
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+
+    fun setPointer(view: View) {
+        nextBtn.setOnClickListener {
+            val skillIntent = Intent(this, SkillActivity::class.java)
+            startActivity(skillIntent)
+        }
+    }
+
 }
